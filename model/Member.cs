e266932da051f9694dcbe19d8d2980072a9124f4
Rasmus Falk <rf222fu@student.lnu.se>
2018-09-20 22:04:07 +0200

    Add member model

---
+++ model/Member.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+
+namespace MemberRegistry.model
+{
+    class Member
+    {
+        private Guid id;
+        private string name;
+        private string personalNumber;
+        private List<Boat> boats;
+    }
+}
