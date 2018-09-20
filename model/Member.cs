46d6dd110630fc677425a3a58da66d16da461512
Rasmus Falk <rf222fu@student.lnu.se>
2018-09-20 22:22:22 +0200

    Add getters

--- model/Member.cs
+++ model/Member.cs
@@ -9,5 +9,18 @@ namespace MemberRegistry.model
         private string name;
         private string personalNumber;
         private List<Boat> boats;
+
+        public Member(string name, string personalNumber)
+        {
+            this.id = Guid.NewGuid();
+            this.name = name;
+            this.personalNumber = personalNumber;
+            this.boats = new List<Boat>();
+        }
+
+        public Guid Id { get { return id; } }
+        public string Name { get { return name; } }
+        public string PersonalNumber { get { return personalNumber; } }
+        public List<Boat> Boats { get { return boats; } }
     }
 }
