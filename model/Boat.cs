11137ae316e416fe3d1d2a2b09c02df9df47e39f
Rasmus Falk <rf222fu@student.lnu.se>
2018-09-29 12:24:44 +0200

    Add validation

--- model/Boat.cs
+++ model/Boat.cs
@@ -4,6 +4,9 @@ namespace MemberRegistry.model
 {
     class Boat
     {
+        private string _type;
+        private double _length;
+
         public Boat(Guid id, string type, double length)
         {
             Id = id;
@@ -12,7 +15,39 @@ namespace MemberRegistry.model
         }
 
         public Guid Id { get; set; }
-        public string Type { get; set; }
-        public double Length { get; set; }
+
+        public string Type
+        {
+            get
+            {
+                return _type;
+            }
+            set
+            {
+                if (value == "")
+                {
+                    throw new ArgumentException("Please enter a type for the boat.");
+                }
+
+                _type = value;
+            }
+        }
+
+        public double Length
+        {
+            get
+            {
+                return _length;
+            }
+            set
+            {
+                if (value == 0)
+                {
+                    throw new ArgumentException("Please enter a length for the boat.");
+                }
+
+                _length = value;
+            }
+        }
     }
 }
--- model/Member.cs
+++ model/Member.cs
@@ -5,6 +5,9 @@ namespace MemberRegistry.model
 {
     class Member
     {
+        private string _name;
+        private string _personalNumber;
+
         public Member(Guid id, string name, string personalNumber)
         {
             Id = id;
@@ -14,8 +17,40 @@ namespace MemberRegistry.model
         }
 
         public Guid Id { get; set; }
-        public string Name { get; set; }
-        public string PersonalNumber { get; set; }
+
+        public string Name
+        {
+            get
+            {
+                return _name;
+            }
+            set
+            {
+                if (value == "")
+                {
+                    throw new ArgumentException("Please enter a name for the member.");
+                }
+
+                _name = value;
+            }
+        }
+
+        public string PersonalNumber
+        {
+            get
+            {
+                return _personalNumber;
+            }
+            set
+            {
+                if (value == "")
+                {
+                    throw new ArgumentException("Please enter a personal number for the member.");
+                }
+
+                _personalNumber = value;
+            }
+        }
         public List<Boat> Boats { get; set; }
     }
 }
